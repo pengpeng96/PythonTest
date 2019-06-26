21224dab70c7de7b59539131952f0ef75cc2ae75
pengpeng96 <48380233+pengpeng96@users.noreply.github.com>
2019-06-26 13:54:19 +0800

    Update lesson_02.py

--- PythonLearning/Lesson02/lesson_02.py
+++ PythonLearning/Lesson02/lesson_02.py
@@ -1,1 +1,31 @@
+'''
+1、用python实现------华氏温度转换为摄氏温度
 
+  # 输入华摄氏度
+  fahrenheit = float(input('输入华氏度:'))
+  celsius = (fahrenheit - 32) / 1.8 
+  # 计算摄氏度
+  print('%.1f华氏度转为摄氏度为%.1f' % (fahrenheit, celsius))
+  
+2、输入圆的半径计算周长和面积
+  
+  import math
+  r=float(input("请输入半径："))
+  circumference = 2*math.pi*r
+  area=math.pi*r*r
+
+  print ( "圆的周长: %.2f" % circumference)
+  print ( "圆的面积: %.2f"% area)
+  
+3、输入年份判断是否是闰年
+  
+
+
+
+
+
+
+
+
+
+'''
