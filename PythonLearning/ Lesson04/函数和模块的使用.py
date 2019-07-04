b34e149f073cb3305a27760aa699596d25c129df
pengpeng96 <48380233+pengpeng96@users.noreply.github.com>
2019-07-04 20:36:04 +0800

    Update 函数和模块的使用.py

--- PythonLearning/ Lesson04/函数和模块的使用.py
+++ PythonLearning/ Lesson04/函数和模块的使用.py
@@ -179,6 +179,33 @@ from module1 import foo
 # 输出hello,world
 foo()
 
+需要说明的是，如果我们导入的模块除了定义函数之外还中有可以执行代码，那么Python解释器在导入这个模块时就会执行这些代码，
+事实上我们可能并不希望如此，因此如果我们在模块中编写了执行代码，最好是将这些执行代码放入如下所示的条件中，这样的话除非
+直接运行该模块，if条件下的这些代码是不会执行的，因为只有直接执行的模块的名字才是“\_\_main\_\_”。
+
+module3.py
+
+def foo():
+    pass
+    
+def bar():
+    pass
+    
+    
+# __name__是Python中一个隐含的变量它代表了模块的名字
+# 只有被Python解释器直接执行的模块的名字才是__main__
+if __name__ == '__main__':
+    print('call foo()')
+    foo()
+    print('call bar()')
+    bar()
+    
+
+test.py
+
+import module3
+
+# 导入module3时，不会执行模块中if成立时的代码，因为模块的名字是module3,而不是__main__
 
 
 '''
