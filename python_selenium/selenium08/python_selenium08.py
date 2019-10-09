42e07689d0a5323f0ae201b0ae059063597e0929
pengpeng96 <48380233+pengpeng96@users.noreply.github.com>
2019-10-09 12:54:51 +0800

    第 9 章课程总结
    第 9 章自动化课程总结

---
+++ python_selenium/selenium08/python_selenium08.py
@@ -0,0 +1,27 @@
+'''
+第九章  课程总结
+
+
+    1、课程总结和常见问题处理
+
+    简介：课程总结和常见的问题处理
+
+        必备：
+        初级：html/css/js/http
+
+        中级：linux、mysql、jmeter(压力测试用的最多的软件之一)、lr(现在用的比较少)
+
+        python + request     网络库去开发接口自动化测试
+
+        高级：jenkins CI、git（管理项目质量、搭建所有基础组件、bug管理工具（bugfree、jira））
+
+
+
+        可选
+        python/java/node
+
+
+        通过百度去解决错误和问题，学会用搜索引擎
+
+
+'''
